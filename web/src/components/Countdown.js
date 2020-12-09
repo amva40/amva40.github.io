387cd08ecc392712007cb49999f23cf11d335ade
Daniel Solarte <danielsolartech@hotmail.com>
2020-12-09 17:33:20 -0500

    fix(WEB): Countdown

--- web/src/components/Countdown.js
+++ web/src/components/Countdown.js
@@ -44,11 +44,11 @@ export default function Countdown() {
     return (
         <animated.div className="countdown" style={boxAnimation}>
             en el 2030 acabaremos con nuestros recursos hídricos si no los gestionamos de manera adecuada:
-            {' ' + time.years} año{time.years === 1 ? '' : 's'},
-            {' ' + time.days} día{time.days === 1 ? '' : 's'},
-            {' ' + time.hours} hora{time.hours === 1 ? '' : 's'},
-            {' ' + time.minutes} minuto{time.minutes === 1 ? '' : 's'} y
-            {' ' + time.seconds} segundo{time.seconds === 1 ? '' : 's'}
+            {` ${(`0${time.years}`.substr(-2))}`} año{time.years === 1 ? '' : 's'},
+            {` ${(`0${time.days}`.substr(-2))}`} día{time.days === 1 ? '' : 's'},
+            {` ${(`0${time.hours}`.substr(-2))}`} hora{time.hours === 1 ? '' : 's'},
+            {` ${(`0${time.minutes}`.substr(-2))}`} minuto{time.minutes === 1 ? '' : 's'} y
+            {` ${(`0${time.seconds}`.substr(-2))}`} segundo{time.seconds === 1 ? '' : 's'}
         </animated.div>
     );
 }
